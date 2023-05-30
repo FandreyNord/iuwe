0398e4001b711f8770f21dd8c8f2dd6b27d849f0
FandreyNord-rss <108738676+FandreyNord@users.noreply.github.com>
2023-05-30 10:38:15 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -36,4 +36,77 @@ left.onclick = function(){
         newElement.style.display = 'flex'
     }
     
+} 
+let geoMap  =document.getElementsByClassName('geo-map')
+let gork = document.getElementsByClassName('gork')
+let chal = document.getElementsByClassName('chal')
+let geo = document.getElementsByClassName('geo')
+let geoCount = 0
+    geo[count-1].onclick = function(){
+    if(geoCount%2==0){
+        geoMap[count-2].style.display = 'block'
+    }
+    else{
+        geoMap[count-2].style.display = 'none'
+    }
+    geoCount++
+}
+
+geo[count].onclick = function(){
+    if(geoCount%2==0){
+        geoMap[count-2].style.display = 'block'
+    }
+    else{
+        geoMap[count-2].style.display = 'none'
+    }
+    geoCount++
+}
+
+geo[count+1].onclick = function(){
+    if(geoCount%2==0){
+        geoMap[count-2].style.display = 'block'
+    }
+    else{
+        geoMap[count-2].style.display = 'none'
+    }
+    geoCount++
+}
+
+geo[count+2].onclick = function(){
+    if(geoCount%2==0){
+        geoMap[count-2].style.display = 'block'
+    }
+    else{
+        geoMap[count-2].style.display = 'none'
+    }
+    geoCount++
+}
+
+geo[count+3].onclick = function(){
+    if(geoCount%2==0){
+        geoMap[count-2].style.display = 'block'
+    }
+    else{
+        geoMap[count-2].style.display = 'none'
+    }
+    geoCount++
+}
+
+geo[count+4].onclick = function(){
+    if(geoCount%2==0){
+        geoMap[count-2].style.display = 'block'
+    }
+    else{
+        geoMap[count-2].style.display = 'none'
+    }
+    geoCount++
+}
+
+chal[0].onclick = function(){
+    document.getElementById('gork').style.display = 'none'
+    document.getElementById('chal').style.display = 'block'
+}
+gork[0].onclick = function(){
+    document.getElementById('chal').style.display = 'none'
+    document.getElementById('gork').style.display = 'block'
 }
